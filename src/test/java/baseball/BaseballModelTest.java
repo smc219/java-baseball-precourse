1a0e5622a1d67ed795948df4bf8c44293273613e
smc219 <seokminchang21@gmail.com>
2021-10-04 16:19:02 +0900

    test(baseBallModel): BaseballModel count ball, strike 기능 테스트 추가
    count ball, count strike기능 추가 커밋에서 test code 부분 누락되어 추가.

--- src/test/java/baseball/BaseballModelTest.java
+++ src/test/java/baseball/BaseballModelTest.java
@@ -1,4 +1,83 @@
 package baseball;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 public class BaseballModelTest {
+
+    BaseballModel baseballModel;
+    BaseballModel spy;
+    @BeforeEach
+    void beforeEach() {
+        baseballModel = new BaseballModel();
+        spy = spy(BaseballModel.class);
+        when(spy.getComputerGeneratedNumber()).thenReturn("123");
+    }
+
+
+
+    @Test
+    void testCheckStrike() {
+        String compared1 = "123";
+        String compared2 = "456";
+        for (int i = 0; i < 3; ++i) {
+            Assertions.assertEquals(1, spy.checkStrike(compared1.charAt(i), i));
+            Assertions.assertEquals(0, spy.checkStrike(compared2.charAt(i), i));
+        }
+    }
+
+    @Test
+    void testCountStrikes(){
+        // 1 strike
+        Assertions.assertEquals(spy.countStrike("145"), 1);
+        Assertions.assertEquals(spy.countStrike("425"), 1);
+        Assertions.assertEquals(spy.countStrike("453"), 1);
+        // 2 strike
+        Assertions.assertEquals(spy.countStrike("125"), 2);
+        Assertions.assertEquals(spy.countStrike("523"), 2);
+        Assertions.assertEquals(spy.countStrike("153"), 2);
+        // 3 strike
+        Assertions.assertEquals(spy.countStrike("123"), 3);
+    }
+
+    @Test
+    void testCountContains() {
+        String oneSameNum = "451";
+        String twoSameNum = "254";
+        String threeSameNum = "435";
+
+        Assertions.assertEquals(1, spy.countContains(oneSameNum.charAt(2)));
+        Assertions.assertEquals(1, spy.countContains(twoSameNum.charAt(0)));
+        Assertions.assertEquals(1, spy.countContains(threeSameNum.charAt(1)));
+
+        Assertions.assertEquals(0, spy.countContains(oneSameNum.charAt(1)));
+        Assertions.assertEquals(0, spy.countContains(twoSameNum.charAt(2)));
+        Assertions.assertEquals(0, spy.countContains(threeSameNum.charAt(0)));
+
+    }
+
+    @Test
+    void testCountBalls() {
+        // no strike
+        String oneSameNum = "451";
+        String twoSameNum = "431";
+        String threeSameNum = "312";
+        // strike
+        String oneStrikeOneBall = "134";
+        String oneStrikeTwoBall = "132";
+
+
+        Assertions.assertEquals(1, spy.countBalls(oneSameNum));
+        Assertions.assertEquals(2, spy.countBalls(twoSameNum));
+        Assertions.assertEquals(3, spy.countBalls(threeSameNum));
+        Assertions.assertEquals(1, spy.countBalls(oneStrikeOneBall));
+        Assertions.assertEquals(2, spy.countBalls(oneStrikeTwoBall));
+
+    }
+
 }
